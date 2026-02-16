74817604181645942160766a6fef5c8f8414378a
Julian Hartline <julianh2o@gmail.com>
2026-02-16 14:18:18 -0800

    adding app title

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
-export const APP_TITLE = 'Template App';
-export const APP_DESCRIPTION = 'A minimal application template';
+import packageJson from '../../package.json';
+
+export const APP_VERSION = packageJson.version;
+export const APP_TITLE = `Outreach - v${APP_VERSION}`;
+export const APP_DESCRIPTION = 'Outreach - Contact management and messaging';
 export const PAGE_TITLE_HOME = 'Home';
